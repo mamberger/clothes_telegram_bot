0ebe6ee59d84203e97f38a041f4c14727b9898e8
kush <kush@beelinerouter.beeline>
2023-03-12 13:23:58 -0400

    new texts, pin start text, start text in db

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,12 +5,15 @@ from utils.api import APIClient as API
 from handlers.users.mixins import mess_delete
 from keyboards.default.client_base import base_markup
 from loader import dp, bot
+from handlers.users.mixins import get_telegram_text
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message, state: FSMContext):
     await mess_delete(state, message.from_user.id)
-    mess = await bot.send_message(message.from_user.id, 'Перезапуск', reply_markup=base_markup)
+    text = get_telegram_text('start')
+    mess = await bot.send_message(message.from_user.id, text, reply_markup=base_markup)
+    bot.pin_chat_message(chat_id=message.from_user.id, message_id=mess.message_id)
     if not await state.get_data():
         API.create_telegram_user(message.from_user.id)
     await state.update_data(sent_messages=[mess.message_id - 1])
